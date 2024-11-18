33d2f5d7dced34136c7e3ef2a709b486d0d427d4
SireneNouira <sirene.nouira16@gmail.com>
2024-11-18 15:38:27 +0100

    ex4fait

---
+++ script.js
@@ -0,0 +1,51 @@
+// EXERCICE 1
+
+// let img = document.querySelector("img");
+// img.addEventListener("mouseover", handleMouseOverAddNewSrc);
+// function handleMouseOverAddNewSrc() {
+
+//     img.src = "images/image1_2.jpg";
+
+// }
+
+// img.addEventListener("mouseout", handleMouseOutAddNewSrc);
+// function handleMouseOutAddNewSrc() {
+
+//     img.src = "images/image1.jpg";
+
+// }
+
+// EXERCICE 2
+
+// let champ= document.querySelector("input");
+
+// champ.addEventListener("blur", handeBoiteDeDia);
+// function handeBoiteDeDia(){
+//     alert("Merci de votre participation");
+// }
+
+// EXERCICE 3
+
+// const champ = document.getElementById("lastname");
+
+// champ.addEventListener("keyup", handleClickOnClavier);
+// function handleClickOnClavier(){
+// let valeur = champ.value;
+//     alert(valeur);
+// }
+
+// EXERCICE 4
+
+// const champ = document.querySelectorAll("input");
+
+// const buttonRaz = document.querySelector("button");
+
+// buttonRaz.addEventListener("click", handleRemoveDonnees);
+// function handleRemoveDonnees() {
+
+//     champ.forEach((element)=> (element.value = ""));
+// }
+
+
+// EXERCICE 5
+
